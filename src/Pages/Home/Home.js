295ae36f867e914a1a5260b54c37985aea493bf0
KshitizAgarwal21 <kshitizagarwal27@gmail.com>
2023-08-03 20:24:23 +0530

    project done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -12,50 +12,6 @@ import BoltIcon from "@mui/icons-material/Bolt";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import axios from "axios";
-const data2 = [
-  {
-    name: "Page A",
-    uv: 590,
-    pv: 800,
-    amt: 1400,
-    cnt: 490,
-  },
-  {
-    name: "Page B",
-    uv: 868,
-    pv: 967,
-    amt: 1506,
-    cnt: 590,
-  },
-  {
-    name: "Page C",
-    uv: 1397,
-    pv: 1098,
-    amt: 989,
-    cnt: 350,
-  },
-  {
-    name: "Page D",
-    uv: 1480,
-    pv: 1200,
-    amt: 1228,
-    cnt: 480,
-  },
-  {
-    name: "Page E",
-    uv: 1520,
-    pv: 1108,
-    amt: 1100,
-    cnt: 460,
-  },
-  {
-    name: "Page F",
-    uv: 1400,
-    pv: 680,
-    amt: 1700,
-    cnt: 380,
-  },
-];
 
 export default function Home() {
   var date = new Date();
@@ -71,6 +27,7 @@ export default function Home() {
   const [value, setValue] = React.useState("Electronics");
 
   const handleChangeTab = (event, newValue) => {
+    console.log(newValue);
     setValue(newValue);
   };
 
@@ -93,9 +50,10 @@ export default function Home() {
   };
 
   const getChart2Data = async () => {
-    const res = await axios.post("http://localhost:8080/sales//chart2Data", {
+    const res = await axios.post("http://localhost:8080/sales/chart2Data", {
       category: value,
     });
+    console.log(res.data);
     setChart2Data(res.data);
     setChart2MainData(res.data);
   };
@@ -110,8 +68,10 @@ export default function Home() {
     }
   }, [units]);
   useEffect(() => {
-    getChart1Data();
     getChart2Data();
+  }, [value]);
+  useEffect(() => {
+    getChart1Data();
   }, []);
 
   return (
@@ -296,9 +256,9 @@ export default function Home() {
           <div style={{ display: "flex", justifyContent: "space-between" }}>
             <p>Sales this Week</p>
             <Tabs value={value} onChange={handleChangeTab} centered>
-              <Tab label="Item One" />
-              <Tab label="Item Two" />
-              <Tab label="Item Three" />
+              <Tab label="Electronics" value="Electronics" />
+              <Tab label="Home Appliances" value="Home Appliances" />
+              <Tab label="Fashion" value="Fashion" />
             </Tabs>
           </div>
           <br></br>
